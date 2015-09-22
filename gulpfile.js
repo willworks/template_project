61c05cdeb9ce1b56d6f18f0fce7963280adda68d
willworks <scnuzhonghanjin@gmail.com>
2015-09-22 20:14:17 +0800

    优化图片处理

--- gulpfile.js
+++ gulpfile.js
@@ -114,7 +114,12 @@ gulp.task('images', function(){
     var imgSrc = './src/images/**/*',
         imgDst = './dist/images';
     gulp.src(imgSrc)
-        .pipe(imagemin())
+        .pipe(imagemin({
+                    optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+                    progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+                    interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+                    multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+         }))
         .pipe(livereload(server))
         .pipe(gulp.dest(imgDst));
 });
